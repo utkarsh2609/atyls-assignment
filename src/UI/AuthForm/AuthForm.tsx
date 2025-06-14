4dfa7ad531d9f330eb2f35fca6e6933d00543f01
utkarsh2609 <utkarsh.awasthi01@gmail.com>
2025-06-14 21:29:25 +0530

    added UI for login form

--- src/UI/AuthForm/AuthForm.tsx
+++ src/UI/AuthForm/AuthForm.tsx
@@ -1,7 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import LoginIcon from "../../assets/images/login.svg";
+import InputField from "../InputField/InputField";
 
 const AuthForm = () => {
+    const signInFormContent = {
+        formHeader: 'Sign in to continue',
+        formSubHeader: 'Sign in to access all the features on this app',
+        formFooter: 'Do not have and account?',
+        footerBtnLable: 'Sign Up',
+    }
+    const [isSignIn, setIsSignIn] = useState(true);
+    const [formContent, setFormContent] = useState(signInFormContent);
+
+    useEffect(() => {
+        if (isSignIn) {
+            setFormContent(signInFormContent);
+        } else {
+            setFormContent({
+                formHeader: 'Create an account to continue',
+                formSubHeader: 'Create an account to access all the features on this app',
+                formFooter: 'Already have an account?',
+                footerBtnLable: 'Sign In',
+            });
+        }
+    } , [isSignIn]);
+
     const [form, setForm] = useState({
         email: "",
         password: "",
@@ -15,59 +38,35 @@ const AuthForm = () => {
                     <div className="w-[53px] h-[53px] rounded-full bg-gray-100 flex items-center justify-center mb-4 shadow">
                         <img src={LoginIcon} alt="" className="w-8 h-8 text-gray-700" />
                     </div>
-                    <h2 className="text-2xl font-bold text-center mt-2 mb-1">Create an account to continue</h2>
+                    <h2 className="text-2xl font-bold text-center mt-2 mb-1">{formContent.formHeader}</h2>
                     <p className="text-gray-400 text-center mb-6 text-base">
-                        Create an account to access all the features on this app
+                        {formContent.formSubHeader}
                     </p>
                 </div>
                 <form className="space-y-4">
-                    <div>
-                        <label className="block font-semibold mb-1 text-gray-900 text-sm">Email or username</label>
-                        <input
-                            type="text"
-                            placeholder="Enter your email or username"
-                            className="w-full rounded-xl bg-gray-100 px-4 py-3 text-base outline-none focus:ring-2 focus:ring-indigo-400"
-                            value={form.email}
-                            onChange={e => setForm({ ...form, email: e.target.value })}
-                        />
-                    </div>
-                    <div>
-                        <label className="block font-semibold mb-1 text-gray-900 text-sm">Password</label>
-                        <input
-                            type="password"
-                            placeholder="Enter your password"
-                            className="w-full rounded-xl bg-gray-100 px-4 py-3 text-base outline-none focus:ring-2 focus:ring-indigo-400"
-                            value={form.password}
-                            onChange={e => setForm({ ...form, password: e.target.value })}
-                        />
-                    </div>
-                    <div>
-                        <label className="block font-semibold mb-1 text-gray-900 text-sm">Repeat password</label>
-                        <input
-                            type="password"
-                            placeholder="Enter your password again"
-                            className="w-full rounded-xl bg-gray-100 px-4 py-3 text-base outline-none focus:ring-2 focus:ring-indigo-400"
-                            value={form.repeatPassword}
-                            onChange={e => setForm({ ...form, repeatPassword: e.target.value })}
-                        />
-                    </div>
+                    <InputField placeholder="Enter your email or username" label="Email or username" value={form.email} onChange={e => setForm({ ...form, email: e.target.value })}/>
+                    <InputField placeholder="Enter your password" label="Password" value={form.password} onChange={e => setForm({ ...form,password: e.target.value })}/>
+                    {
+                        !isSignIn && 
+                        <InputField placeholder="Enter your password again" label="Repeat password" value={form.repeatPassword} onChange={e => setForm({ ...form, repeatPassword: e.target.value })}/>
+                    }
                     <button
                         type="submit"
-                        className="w-full mt-2 bg-indigo-600 hover:bg-indigo-700 text-white font-semibold rounded-xl py-3 text-base transition"
+                        className="w-full mt-2 bg-indigo-600 hover:bg-indigo-700 text-white font-semibold rounded-xl py-3 text-base transition cursor-pointer"
                     >
-                        Sign Up
+                        {isSignIn ? 'Sign In' : 'Sign Up'}
                     </button>
                 </form>
             </div>
             <div className="text-center text-gray-400 mt-3.5 mb-1 text-base">
-                Already have an account?
+                {formContent.formFooter}
                 <button
-                    className="ml-1 text-indigo-600 font-semibold hover:underline"
+                    className="ml-1 text-indigo-600 font-semibold hover:underline cursor-pointer"
                     type="button"
-                    onClick={() => { console.log("Switch to Sign In") }
+                    onClick={() => { setIsSignIn(prev => !prev) }
                     }
                 >
-                    Sign In
+                    {formContent.footerBtnLable}
                 </button>
             </div>
         </div>
--- src/UI/Header/Header.tsx
+++ src/UI/Header/Header.tsx
@@ -1,11 +1,17 @@
+import LoginIcon from '../../assets/images/login.svg';
+import MouseIcon from '../../assets/images/mouse.svg';
+
 const Header = () => {
   return (
     <header className="w-full text-gray-800 p-2 flex items-center justify-between bg-white shadow-md fixed">
-      <h3 className="text-base font-semibold">foo-rum</h3>
+      <div className="flex gap-2 items-center">
+        <img src={MouseIcon} alt="" className="w-5 h-5 text-gray-700" />
+        <h3 className="text-base font-semibold">foo-rum</h3>
+      </div>
       <nav>
-        <div className="flex gap-2.5 font-medium p-1">
+        <div className="flex items-center gap-2.5 font-medium p-1">
           Login
-
+          <img src={LoginIcon} alt="" className="w-5 h-5 text-gray-700" />
         </div>
       </nav>
     </header>
