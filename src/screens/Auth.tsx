87f8494b2858aad0827fc2fa909d7c32ce2a758b
utkarsh2609 <utkarsh.awasthi01@gmail.com>
2025-06-15 13:37:31 +0530

    removed modal from login route

--- src/screens/Auth.tsx
+++ src/screens/Auth.tsx
@@ -1,12 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import AuthForm from "../UI/AuthForm/AuthForm";
-import Modal from "../UI/Modal/Modal";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router";
 
 const Auth = () => {
     const navigate = useNavigate();
-    const [isOpen, setIsOpen] = useState(false);
     const { user } = useAuth();
 
 
@@ -15,19 +13,13 @@ const Auth = () => {
         if (user) {
             // If user is already authenticated, redirect to home
             navigate('/');
-        } else {
-            setIsOpen(true);
         }
     }, [user]);
 
 
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-2xl font-bold mb-4">Authentication</h1>
-            <p className="text-gray-600 mb-6">Please log in to continue.</p>
-            <Modal showCloseBtn={true} isOpen={isOpen} onClose={() => setIsOpen(false)}>
-                <AuthForm />
-            </Modal>
+            <AuthForm />
         </div>
     );
 }
